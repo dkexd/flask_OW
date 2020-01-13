a25c393faa38e64def3d8135e266cabe5dde4a83
Luna <yobapeka1@gmail.com>
2020-01-13 03:02:40 +0300

    Python Flask Tutorial: Full-Featured Web App Part 2 - Templates https://www.youtube.com/watch?v=QnDWIZuWYW0

--- flaskblog.py
+++ flaskblog.py
@@ -2,5 +2,17 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+#  2nd route for same webpage
+@app.route('/home')
 def hello_world():
     return '<h1>Hello, World!</h1>'
+
+#  new webpage
+@app.route('/about')
+def about():
+    return '<h1>About</h1>'
+
+#  for running as 'python flaskblog.py'
+#  intead of 'flask run' w/ setting loacl variable through set/export
+if __name__ == '__main__':
+    app.run(debug=True)
