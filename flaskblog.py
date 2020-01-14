e2c7658f17f3f8804d7893cca89c3108a5522b41
Luna <yobapeka1@gmail.com>
2020-01-14 22:41:51 +0300

    + bootstrap; + layout.html template; + css;

--- flaskblog.py
+++ flaskblog.py
@@ -1,18 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'me',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Ivan',
+        'title': 'Blog Post 2',
+        'content': 'Blog post 2',
+        'date_posted': 'April 21, 2018'
+    }
+]
 
 @app.route('/')
 #  2nd route for same webpage
 @app.route('/home')
-def hello_world():
-    return '<h1>Hello, World!</h1>'
+def home():
+    return render_template('home.html', posts=posts)
 
 #  new webpage
 @app.route('/about')
 def about():
-    return '<h1>About</h1>'
+    return render_template('about.html', title='About')
 
 #  for running as 'python flaskblog.py'
-#  intead of 'flask run' w/ setting loacl variable through set/export
+#  instead of 'flask run' w/ setting local variable through set/export
 if __name__ == '__main__':
     app.run(debug=True)
